24f4ade8baf0f58f1a56e79d0bb32c69779613ca
Luiz Paulo <lppjunior@gmail.com>
2020-07-30 00:46:41 -0300

    - Add Observer parameter validations - Add Observer tests - Update jest to 100% code coverage

--- src/pattern/observer/Observer.js
+++ src/pattern/observer/Observer.js
@@ -4,17 +4,42 @@ class Observer {
   }
 
   on (event, callback) {
-    if (this.observer[event] === undefined) {
-      this.observer[event] = []
-    }
-
+    this.checkParam('callback', callback)
+    this.addEvent(event)
     this.observer[event].push(callback)
 
     return this
   }
 
   emit (event, data) {
+    this.addEvent(event)
     this.observer[event].forEach(callback => callback(data))
+  }
+
+  addEvent(event) {
+    this.checkParam('event', event)
+    if (this.observer[event] === undefined) {
+      this.observer[event] = []
+    }
+  }
+
+  checkParam(param, value) {
+    switch (param) {
+      case 'event':
+        if (!(typeof value === 'string' || value instanceof String)) {
+          throw new Error('Event definition must be a string')
+        }
+        break;
+
+        case 'callback':
+          if (!(value && {}.toString.call(value) === '[object Function]')) {
+            throw new Error('callback definition must be a function')
+          }
+          break;
+
+      default:
+        throw new Error('Inexistent parameter')
+    }
   }
 }
 
