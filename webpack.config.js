b2715f0af52bd18b5e31f82aaa2257d458534692
Luiz Paulo <lppjunior@gmail.com>
2020-08-25 15:03:29 -0300

    Update webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,61 @@ const CopyPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
-module.exports = {
-  entry: {
-    patterns: './src'
-  },
-  output: {
-    filename: 'src/[name].min.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'umd'
-  },
-  devtool: 'source-map',
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Pattern JS',
-      favicon: 'src-example/favicon.png',
-      template: 'src-example/index.html'
-    }),
-    new CopyPlugin({
-      patterns: [
-        path.resolve(__dirname, 'src-example', 'src')
-      ]
-    })
-  ],
+const config = {}
 
-  // webpack-dev-server configurations
-  devServer: {
-    compress: true,
-    contentBase: path.join(__dirname, 'dist'),
-    open: true,
-    port: 9000
+const process = [
+  {
+    ...config,
+    name: "all",
+    entry: {
+      patterns: './src'
+    },
+    output: {
+      filename: 'src/[name].min.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'umd'
+    }
+  },
+  {
+    ...config,
+    name: "unity",
+    entry: {
+      middleware: './src/pattern/middleware',
+      pipeline: './src/pattern/pipeline',
+      observer: './src/pattern/observer'
+    },
+    output: {
+      filename: 'src/pattern/[name].min.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'umd'
+    }
   }
-}
+]
+
+module.exports = [
+  ...process,
+  {
+    devtool: 'source-map',
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Pattern JS',
+        favicon: 'src-example/favicon.png',
+        template: 'src-example/index.html'
+      }),
+      new CopyPlugin({
+        patterns: [
+          path.resolve(__dirname, 'src-example', 'src')
+        ]
+      })
+    ],
+
+    // webpack-dev-server configurations
+    devServer: {
+      compress: true,
+      contentBase: path.join(__dirname, 'dist'),
+      open: true,
+      port: 9000
+    }
+  }
+]
