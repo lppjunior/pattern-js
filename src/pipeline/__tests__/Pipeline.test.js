187f06e3acfee65432e5bfee91d2a0cf646cb1d1
Luiz Paulo <lppjunior@gmail.com>
2020-09-01 01:06:14 -0300

    Review tests

--- src/pipeline/__tests__/Pipeline.test.js
+++ src/pipeline/__tests__/Pipeline.test.js
@@ -1,40 +1,57 @@
 import Pipeline from '../Pipeline'
 
-describe('Test Pipeline call all methods', () => {
+describe('Pipeline', () => {
   const pipeline = new Pipeline()
 
-  test('should assert method pipeline.pipe was called', () => {
-    jest.spyOn(pipeline, 'pipe')
-    pipeline.pipe(() => {})
-    expect(pipeline.pipe).toHaveBeenCalled()
+  describe('Test existance of all public methods and attributes', () => {
+    test('should assert than pipeline has been defined', () => {
+      expect(pipeline).toBeDefined()
+    })
+
+    test('should assert than pipe method has been defined', () => {
+      expect(pipeline.pipe).toBeDefined()
+    })
+
+    test('should assert than process method has been defined', () => {
+      expect(pipeline.process).toBeDefined()
+    })
   })
 
-  test('should assert method pipeline.process was called', () => {
-    jest.spyOn(pipeline, 'process')
-    pipeline.process(() => {})
-    expect(pipeline.process).toHaveBeenCalled()
+  describe('Test Pipeline call all methods', () => {
+
+    test('should assert method pipeline.pipe was called', () => {
+      jest.spyOn(pipeline, 'pipe')
+      pipeline.pipe(() => {})
+      expect(pipeline.pipe).toHaveBeenCalled()
+    })
+
+    test('should assert method pipeline.process was called', () => {
+      jest.spyOn(pipeline, 'process')
+      pipeline.process(() => {})
+      expect(pipeline.process).toHaveBeenCalled()
+    })
   })
-})
 
-describe('Test Pipeline call perfect scenaries', () => {
-  const result = (new Pipeline())
-    .pipe((data) => {
-      test('Data should be 5', () => {
-        expect(data).toBe(5)
+  describe('Test Pipeline call perfect scenaries', () => {
+    const result = (new Pipeline())
+      .pipe((data) => {
+        test('Data should be 5', () => {
+          expect(data).toBe(5)
+        })
+
+        return data + 5
       })
+      .pipe((data) => {
+        test('Data should  be 10', () => {
+          expect(data).toBe(10)
+        })
 
-      return data + 5
-    })
-    .pipe((data) => {
-      test('Data should  be 10', () => {
-        expect(data).toBe(10)
+        return data * 2
       })
+      .process(5)
 
-      return data * 2
+    test('Result should be 20', () => {
+      expect(result).toBe(20)
     })
-    .process(5)
-
-  test('Result should be 20', () => {
-    expect(result).toBe(20)
   })
 })
